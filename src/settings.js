d8dfefb079048ab394904f26d5f3408e4f6e310c
[username] <997247807@qq.com>
2021-06-01 21:03:50 +0800

    第一次提交

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  /**
+   * 标题栏的显示文字
+   */
+  title: '物联网云平台',
+
+  /**
+   * 固定NavBar头部，不随滚动条滚动消失
+   */
+  fixedHeader: true,
+
+  /**
+   * SideBar的logo是否显示
+   */
+  sidebarLogo: true
+}
